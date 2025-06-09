2d51fbdd8a5f0577cecab5d1e05dd6a80749d48f
Robert Stein <rdstein@umd.edu>
2025-06-09 14:51:29 -0400

    Add a test

---
+++ tests/test_run.py
@@ -0,0 +1,52 @@
+"""
+Module for testing galsynthspec
+"""
+
+import logging
+import unittest
+from pathlib import Path
+
+import pandas as pd
+from click.testing import CliRunner
+
+from galsynthspec.cli.wrappers import cli
+from galsynthspec.utils.query import query_by_name
+
+logger = logging.getLogger(__name__)
+
+expected_df = pd.read_json(
+    Path(__file__).parent / "test_data/synthetic_photometry.json"
+)
+
+
+class TestGalsynthspec(unittest.TestCase):
+    """
+    Class for testing Galsynthspec
+    """
+
+    def test_by_name(self):
+        """
+        Test ping
+
+        :return: None
+        """
+
+        test_name = "ZTF19aapreis"
+
+        logger.info(f"Testing Galsynthspec for {test_name}")
+
+        runner = CliRunner()
+        result = runner.invoke(
+            cli,
+            ["by-name", test_name],
+        )
+        assert result.exit_code == 0
+
+        # Check results
+
+        logger.info(f"Checking results for {test_name}")
+
+        gal = query_by_name(test_name)
+        assert gal.synthetic_photometry_file.exists()
+        new = pd.read_json(gal.synthetic_photometry_file)
+        pd.testing.assert_frame_equal(new, expected_df)
